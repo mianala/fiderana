c7e70e943c09ecb2ee3274784a2f6c6b552b2974
Loharano <mianalaloharano@gmail.com>
2021-10-21 10:21:12 +0300

    Added navigation

--- app/src/main/java/com/mianala/fiderana/MainActivity.kt
+++ app/src/main/java/com/mianala/fiderana/MainActivity.kt
@@ -3,13 +3,23 @@ package com.mianala.fiderana
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.mianala.fiderana.ui.theme.FideranaTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,11 +28,42 @@ class MainActivity : ComponentActivity() {
         setContent {
             FideranaTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                val navController = rememberNavController()
+                Scaffold(
+                    bottomBar = {
+                        NavigationBar(
+                            items = listOf(
+                                NavItem(
+                                    name = "Songs",
+                                    route = "songs",
+                                    icon = Icons.Filled.Audiotrack
+                                ),
+                                NavItem(
+                                    name = "Dial",
+                                    route = "dial",
+                                    icon = Icons.Filled.Pin
+                                ),
+                                NavItem(
+                                    name = "Song",
+                                    route = "song",
+                                    icon = Icons.Filled.PlayCircle
+                                ),
+                                NavItem(
+                                    name = "Category",
+                                    route = "category",
+                                    icon = Icons.Filled.EmojiPeople
+                                ),
+                                NavItem(
+                                    name = "Playlist",
+                                    route = "playlist",
+                                    icon = Icons.Filled.PlaylistPlay
+                                ),
+                            ),
+                            navController = navController,
+                            onItemclick = { navController.navigate(it.route) }
+                        )
+                    }) {
+                    Navigation(navController = navController)
                 }
             }
         }
@@ -30,14 +71,137 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun NavigationBar(
+    items: List<NavItem>,
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    onItemclick: (NavItem) -> Unit
+) {
+    val backStackEntry = navController.currentBackStackEntryAsState()
+
+    BottomNavigation(
+        modifier = modifier,
+        backgroundColor = Color.White,
+        elevation = 5.dp
+    ) {
+        items.forEach { item ->
+            val selected = item.route == backStackEntry.value?.destination?.route
+            BottomNavigationItem(
+                selected = selected,
+                onClick = { onItemclick(item) },
+                selectedContentColor = Color.Red,
+                unselectedContentColor = Color.DarkGray,
+                icon = {
+                    Icon(imageVector = item.icon, contentDescription = item.name)
+                }
+            )
+
+        }
+    }
+}
+
+
+@Composable
+fun Navigation(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = "dial") {
+        composable("dial") {
+            DialScreen()
+        }
+        composable("song") {
+            SongScreen()
+        }
+        composable("settings") {
+            SettingsScreen()
+        }
+        composable("category") {
+            CategoryScreen()
+        }
+        composable("playlist") {
+            PlaylistScreen()
+        }
+        composable("authors") {
+            AuthorsScreen()
+        }
+        composable("songs") {
+            SongsScreen()
+        }
+    }
+}
+
+@Composable
+fun DialScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("Dial")
+    }
+}
+
+@Composable
+fun SongsScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("Songs")
+    }
+}
+
+@Composable
+fun SongScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("Song")
+    }
+}
+
+@Composable
+fun PlaylistScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("Playlist")
+    }
+}
+
+@Composable
+fun CategoryScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("Category")
+    }
+}
+
+@Composable
+fun SettingsScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("Settings")
+    }
+}
+
+@Composable
+fun AuthorsScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("Authors")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     FideranaTheme {
-        Greeting("Android")
+        AuthorsScreen()
     }
 }
